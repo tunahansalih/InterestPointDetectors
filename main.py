fb2e05f971781e5e214cb2764605313bf8f3c432
tunahansalih <tusame1994>
2018-11-27 01:36:20 +0300

    Homework done

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import os
 
 def read_image(filename):
     image = cv2.imread(filename)
-    show_image(image)
+    if image is None:
+        print("Error reading")
+    # show_image(image)
     return image
 
 
@@ -39,41 +41,36 @@ def draw_keypoints(image, keypoints, label=None):
 
 
 def myHarrisCornerDetector(image):
-    k = 0.04
-    window_size = 5
-    dx = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=3)
-    dy = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=3)
-    show_image(dx, gray=True)
-    show_image(dy, gray=True)
+    k = 0.06
+    window_size = 3
+    threshold = 250
+    x_gradient_kernel = np.zeros((1, 3))
+    x_gradient_kernel[0, 0] = -1
+    x_gradient_kernel[0, 2] = 1
+    y_gradient_kernel = np.zeros((3, 1))
+    y_gradient_kernel[0, 0] = -1
+    y_gradient_kernel[2, 0] = 1
+    dx = cv2.filter2D(image, -1, x_gradient_kernel)
+    dy = cv2.filter2D(image, -1, y_gradient_kernel)
+    # show_image(dx, gray=True)
+    # show_image(dy, gray=True)
 
     Ixx = dx ** 2
     Ixy = dx * dy
     Iyy = dy ** 2
-    image_height = image.shape[0]
-    image_width = image.shape[1]
     corners = []
-    r_values = np.zeros_like(image, dtype=np.float32)
-    offset = window_size // 2
 
-    for y in range(offset, image_height - offset):
-        for x in range(offset, image_width - offset):
-            windowIxx = Ixx[y - offset:y + offset + 1, x - offset:x + offset + 1]
-            windowIxy = Ixy[y - offset:y + offset + 1, x - offset:x + offset + 1]
-            windowIyy = Iyy[y - offset:y + offset + 1, x - offset:x + offset + 1]
-            sumIxx = np.sum(windowIxx)
-            sumIxy = np.sum(windowIxy)
-            sumIyy = np.sum(windowIyy)
+    sumIxx = cv2.filter2D(Ixx, -1, np.ones((window_size, window_size)))
+    sumIxy = cv2.filter2D(Ixy, -1, np.ones((window_size, window_size)))
+    sumIyy = cv2.filter2D(Iyy, -1, np.ones((window_size, window_size)))
 
-            M = np.array([np.array([sumIxx, sumIxy]),
-                          np.array([sumIxy, sumIyy])])
+    det_m = sumIxx * sumIyy - sumIxy * sumIxy
+    trace_m = (sumIxx + sumIyy)
+    r_values = det_m - k * (trace_m ** 2)
 
-            r = np.linalg.det(M) - k * (np.trace(M) ** 2)
-            r_values[y, x] = r
-
-    max_r_val = np.max(r_values)
     for y in range(r_values.shape[0]):
         for x in range(r_values.shape[1]):
-            if r_values[y, x] > 0.01 * max_r_val:
+            if r_values[y, x] > threshold:
                 corners.append([x, y])
     return corners
 
@@ -120,18 +117,22 @@ def get_harris_sift_surf_points(image):
     sift_points, sift_keypoints = get_corner_points(image, algorithm='sift')
     surf_points, surf_keypoints = get_corner_points(image, algorithm='surf')
 
-    return harris_points, sift_points, sift_keypoints, surf_points, surf_keypoints
+    return np.array(harris_points), np.array(sift_points), sift_keypoints, np.array(surf_points), surf_keypoints
 
 
-def measureRepeatability(keyPoints1, keyPoints2, homography1to2=np.fill_diagonal(np.zeros((3, 3)), 1), image2size=None):
+def measureRepeatability(keyPoints1, keyPoints2, homography1to2=np.diag(np.ones(3)), image2size=None):
     num_of_points_with_neighbours = 0
     img2 = np.zeros(image2size)
+    keyPoints2 = cv2.perspectiveTransform(keyPoints2.reshape(-1, 1, 2).astype(np.float32), homography1to2)
+    keyPoints2 = keyPoints2.reshape(-1, 2).astype(np.int32)
     for kp2 in keyPoints2:
-        img2[kp2[1], kp2[0]] = 1
+        if img2.shape[0] > kp2[1] >= 0 and img2.shape[1] > kp2[0] >= 0:
+            img2[kp2[1], kp2[0]] = 1
 
     for kp1 in keyPoints1:
-        if np.sum(img2[kp1[1] - 1:kp1[1] + 2, kp1[0] - 1:kp1[0] + 2]) > 0:
-            num_of_points_with_neighbours += 1
+        if img2.shape[0] - 2 > kp1[1] and kp1[1] >= 1 and img2.shape[1] - 2 > kp1[0] and kp1[0] >= 1:
+            if np.sum(img2[kp1[1] - 1:kp1[1] + 2, kp1[0] - 1:kp1[0] + 2]) > 0:
+                num_of_points_with_neighbours += 1
 
     repeatability = num_of_points_with_neighbours / np.min((len(keyPoints1), len(keyPoints2)))
     return repeatability
@@ -139,28 +140,116 @@ def measureRepeatability(keyPoints1, keyPoints2, homography1to2=np.fill_diagonal
 
 graffiti_image_files = [f'Images/img{i}.png' for i in [1, 2, 3, 4, 5, 6]]
 noisy_image_files = [f'Images/kuzey_noise_mean_{x}.jpeg' for x in [5, 10, 20, 50, 100, 200]]
-jpeg_image_files = [f'Images/kuzey_quality_{quality}.jpeg' for quality in np.linspace(0, 100, num=6, endpoint=True)]
+jpeg_image_files = [f'Images/kuzey_quality_{quality}.jpeg' for quality in [0, 20, 40, 60, 20, 100]]
 homography_matrix_files = [f'Images/H1to{x}p' for x in range(2, 7)]
-#
-# filename = "Images/kuzey.jpg"
-# create_different_quality_images(filename)
-# create_different_images_with_gaussian(filename)
-#
-# image_bgr = read_image(filename)
-# image_gray = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2GRAY)
-#
-# harris_points, sift_points, sift_keypoints, surf_points, surf_keypoints = get_harris_sift_surf_points(image_gray)
-# draw_points(image_bgr, harris_points, label='harris')
-# draw_keypoints(image_bgr, sift_keypoints, label='sift')
-# draw_keypoints(image_bgr, surf_keypoints, label='surf')
 
+filename = "Images/kuzey.jpg"
+create_different_quality_images(filename)
+create_different_images_with_gaussian(filename)
 
-img1 = read_image(noisy_image_files[0])
+img1 = read_image(filename)
 img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 harris_points1, sift_points1, sift_keypoints1, surf_points1, surf_keypoints1 = get_harris_sift_surf_points(img1_gray)
-for i in range(1, len(noisy_image_files)):
+
+image_with_points = img1.copy()
+
+for x, y in harris_points1:
+    image_with_points[y, x] = [0, 0, 255]
+
+image_rgb = cv2.cvtColor(image_with_points, cv2.COLOR_BGR2RGB)
+plt.imshow(image_rgb)
+plt.title("Image with Detected Corners Using Harris Algorithm")
+plt.savefig('Images/harris_corners.png')
+
+image_with_keypoints = cv2.drawKeypoints(img1, sift_keypoints1, None)
+img_rgb = cv2.cvtColor(image_with_points, cv2.COLOR_BGR2RGB)
+plt.imshow(img_rgb)
+plt.title("Image with Detected Corners Using SIFT algorithm")
+plt.savefig('Images/sift_corners.png')
+
+image_with_keypoints = cv2.drawKeypoints(img1, surf_keypoints1, None)
+img_rgb = cv2.cvtColor(image_with_points, cv2.COLOR_BGR2RGB)
+plt.imshow(img_rgb)
+plt.title("Image with Detected Corners Using SURF algorithm")
+plt.savefig('Images/surf_corners.png')
+
+noisy_repeatability = {'harris': [], 'sift': [], 'surf': []}
+for i in range(len(noisy_image_files)):
     img2 = read_image(noisy_image_files[i])
     img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
     harris_points2, sift_points2, sift_keypoints2, surf_points2, surf_keypoints2 = get_harris_sift_surf_points(
         img2_gray)
-    print(i, measureRepeatability(keyPoints1=harris_points1, keyPoints2=harris_points2, image2size=img2_gray.shape))
+
+    harris_repeatability = measureRepeatability(keyPoints1=harris_points1, keyPoints2=harris_points2,
+                                                image2size=img2_gray.shape)
+    sift_repeatability = measureRepeatability(keyPoints1=sift_points1, keyPoints2=sift_points2,
+                                              image2size=img2_gray.shape)
+    surf_repeatability = measureRepeatability(keyPoints1=surf_points1, keyPoints2=surf_points2,
+                                              image2size=img2_gray.shape)
+    noisy_repeatability['harris'].append(harris_repeatability)
+    noisy_repeatability['sift'].append(sift_repeatability)
+    noisy_repeatability['surf'].append(surf_repeatability)
+
+jpeg_repeatability = {'harris': [], 'sift': [], 'surf': []}
+for i in range(len(jpeg_image_files)):
+    img2 = read_image(jpeg_image_files[i])
+    img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    harris_points2, sift_points2, sift_keypoints2, surf_points2, surf_keypoints2 = get_harris_sift_surf_points(
+        img2_gray)
+    harris_repeatability = measureRepeatability(keyPoints1=harris_points1, keyPoints2=harris_points2,
+                                                image2size=img2_gray.shape)
+    sift_repeatability = measureRepeatability(keyPoints1=sift_points1, keyPoints2=sift_points2,
+                                              image2size=img2_gray.shape)
+    surf_repeatability = measureRepeatability(keyPoints1=surf_points1, keyPoints2=surf_points2,
+                                              image2size=img2_gray.shape)
+    jpeg_repeatability['harris'].append(harris_repeatability)
+    jpeg_repeatability['sift'].append(sift_repeatability)
+    jpeg_repeatability['surf'].append(surf_repeatability)
+
+img1 = read_image(graffiti_image_files[0])
+img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+harris_points1, sift_points1, sift_keypoints1, surf_points1, surf_keypoints1 = get_harris_sift_surf_points(img1_gray)
+
+graffiti_repeatability = {'harris': [], 'sift': [], 'surf': []}
+for i in range(1, len(graffiti_image_files)):
+    img2 = read_image(graffiti_image_files[i])
+    img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    harris_points2, sift_points2, sift_keypoints2, surf_points2, surf_keypoints2 = get_harris_sift_surf_points(
+        img2_gray)
+    homography_matrix = np.loadtxt(homography_matrix_files[i - 1])
+    harris_repeatability = measureRepeatability(keyPoints1=harris_points1, keyPoints2=harris_points2,
+                                                image2size=img2_gray.shape, homography1to2=homography_matrix)
+    sift_repeatability = measureRepeatability(keyPoints1=sift_points1, keyPoints2=sift_points2,
+                                              image2size=img2_gray.shape, homography1to2=homography_matrix)
+    surf_repeatability = measureRepeatability(keyPoints1=surf_points1, keyPoints2=surf_points2,
+                                              image2size=img2_gray.shape, homography1to2=homography_matrix)
+    graffiti_repeatability['harris'].append(harris_repeatability)
+    graffiti_repeatability['sift'].append(sift_repeatability)
+    graffiti_repeatability['surf'].append(surf_repeatability)
+
+for k, v in jpeg_repeatability.items():
+    jpeg_rep = jpeg_repeatability[k]
+    plt.plot(['0-1', '0-2', '0-3', '0-4', '0-5', '0-6'], jpeg_rep, '-o')
+    plt.xlabel('Compared Images')
+    plt.ylabel('Repeatability')
+    plt.ylim([0, 1])
+    plt.title(f'{k} Corner Detection on Different JPEG Qualities')
+    plt.savefig(f'Images/{k}_jpeg.svg')
+
+for k, v in noisy_repeatability.items():
+    jpeg_rep = noisy_repeatability[k]
+    plt.plot(['0-5', '0-10', '0-20', '0-50', '0-100', '0-200'], jpeg_rep, '-o')
+    plt.xlabel('Compared Images')
+    plt.ylabel('Repeatability')
+    plt.ylim([0, 1])
+    plt.title(f'{k} Corner Detection on Different Gaussian Noise')
+    plt.savefig(f'Images/{k}_noisy.svg')
+
+for k, v in graffiti_repeatability.items():
+    jpeg_rep = graffiti_repeatability[k]
+    plt.plot(['1-2', '1-3', '1-4', '1-5', '1-6'], jpeg_rep, '-o')
+    plt.xlabel('Compared Images')
+    plt.ylabel('Repeatability')
+    plt.ylim([0, 1])
+    plt.title(f'{k} Corner Detection on Homography Transformation')
+    plt.savefig(f'Images/{k}_homography.svg')
